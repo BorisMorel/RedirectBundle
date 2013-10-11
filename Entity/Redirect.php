b7095c27cf846da5151823c1cb659788c8bf796f
Boris Morel <Boris.Morel@imag.fr>
2013-10-11 16:01:27 +0200

    Working subscriber

---
+++ Entity/Redirect.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace BOMO\RedirectBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="BOMO\RedirectBundle\Repository\RedirectRepository")
+ * @ORM\Table(name="Redirect")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Redirect
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @Assert\Type("integer")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Type("string")
+     * @Assert\Url(message="redirect.url_source.invalid")
+     * @Assert\Length(max=255)
+     * @Assert\NotBlank(message="redirect.url_source.not_blank")
+     */
+    protected $urlSource;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Type("string")
+     * @Assert\Url(message="redirect.url_target.invalid")
+     * @Assert\Length(max=255)
+     * @Assert\NotBlank(message="redirect.url_target.not_blank")
+     */
+    protected $urlTarget;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Type("string")
+     * @Assert\Length(max=255)
+     * @Assert\NotBlank(message="redirect.code.not_blank")
+     */
+    protected $code;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @Assert\Type("bool")
+     */
+    protected $isActive;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set urlSource
+     *
+     * @param string $urlSource
+     * @return Redirect
+     */
+    public function setUrlSource($urlSource)
+    {
+        $this->urlSource = $urlSource;
+    
+        return $this;
+    }
+
+    /**
+     * Get urlSource
+     *
+     * @return string 
+     */
+    public function getUrlSource()
+    {
+        return $this->urlSource;
+    }
+
+    /**
+     * Set urlTarget
+     *
+     * @param string $urlTarget
+     * @return Redirect
+     */
+    public function setUrlTarget($urlTarget)
+    {
+        $this->urlTarget = $urlTarget;
+    
+        return $this;
+    }
+
+    /**
+     * Get urlTarget
+     *
+     * @return string 
+     */
+    public function getUrlTarget()
+    {
+        return $this->urlTarget;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     * @return Redirect
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string 
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return Redirect
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+    
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean 
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+}
---
+++ EventSubscriber/RedirectSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BOMO\RedirectBundle\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class RedirectSubscriber implements EventSubscriberInterface
+{
+    static public function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::REQUEST => array(
+                array('checkRedirect', 0),
+            ),
+        );
+    }
+
+    public function __construct(\Doctrine\ORM\EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function checkRedirect(GetResponseEvent $event)
+    {
+        try {
+            $url = $this->em
+                ->getRepository('BOMORedirectBundle:Redirect')
+                ->getFromSource($event->getRequest()->getPathInfo())
+                ;
+            
+            $reponse = new RedirectResponse($url->getUrlTarget(), $url->getCode());
+                
+            $event->setResponse($reponse);
+
+        } catch (\Doctrine\Orm\NoResultException $exception) {
+            return;
+            
+        }
+    }
+}
